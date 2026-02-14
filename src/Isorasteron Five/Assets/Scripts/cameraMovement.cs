4661325bb01d3c82992f48f4f1352bad357d3065
CentreFusion <calvinaawebb@gmail.com>
2026-02-14 15:45:51 -0700

    Basic player movement setup
    Just made the player move very simply, still not done with the player system but committing for collaborators.

---
+++ src/Isorasteron Five/Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    public GameObject camera;
+    public GameObject player;
+    void Start()
+    {
+        camera.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 2f, player.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (camera != null)
+        {
+            Vector3 velocity = new Vector3(0, 0, 0);
+            camera.transform.position = Vector3.SmoothDamp(camera.transform.position, player.transform.position, ref velocity, 0.05f);
+        }
+    }
+}
