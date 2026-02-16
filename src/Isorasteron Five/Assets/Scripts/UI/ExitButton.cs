72aa22b235fbaa0f15c1ae1bf710991018d0d438
Captain Cobbs <egamezhq@gmail.com>
2026-02-16 16:18:22 -0700

    Fixed a bunch of random stuff, added button scripts

---
+++ src/Isorasteron Five/Assets/Scripts/UI/ExitButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    [RequireComponent(typeof(Button))]
+    public class ExitButton : MonoBehaviour
+    {
+        private void Awake()
+        {
+            if (TryGetComponent(out Button button))
+            {
+                button.onClick.AddListener(ExitGame);
+            }
+        }
+
+        private void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
